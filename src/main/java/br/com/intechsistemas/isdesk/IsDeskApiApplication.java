e5c31649a91f5f04806950343188dbbade4edb0d
Pablo Lopes <intechsistemas.dev@gmail.com>
2021-05-04 14:17:16 -0300

    Iniciando Projeto

---
+++ src/main/java/br/com/intechsistemas/isdesk/IsDeskApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.intechsistemas.isdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsDeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsDeskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/intechsistemas/isdesk/IsDeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.intechsistemas.isdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsDeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
